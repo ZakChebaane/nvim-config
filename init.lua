8850fca9d4597fc5187f16d1bd400c90b4f53428
ZakChebaane <150349051+ZakChebaane@users.noreply.github.com>
2026-09-05 20:35:44 +0100

    color scheme setup -> nightfly

--- init.lua
+++ init.lua
@@ -335,16 +335,14 @@ do
   -- change the command under that to load whatever the name of that colorscheme is.
   --
   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  vim.pack.add { gh 'catppuccin/nvim.git' }
+  vim.pack.add { gh 'bluz71/vim-nightfly-colors' }
   ---@diagnostic disable-next-line: missing-fields
-  require('catppuccin').setup {
-    flavour = "auto",
-  }
+  -- require('oxocarbon').setup {}
 
   -- Load the colorscheme here.
   -- Like many other themes, this one has different styles, and you could load
   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-  vim.cmd.colorscheme 'catppuccin-macchiato'
+  vim.cmd.colorscheme 'nightfly'
 
   -- Highlight todo, notes, etc in comments
   vim.pack.add { gh 'folke/todo-comments.nvim' }
